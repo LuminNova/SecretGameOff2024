c4275a45530abbd7dfcc27f163880eea08427086
Anthony Dang <anthonydang230@gmail.com>
2024-11-12 15:40:32 -0700

    added new character and slowly configuring movement animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,11 +5,15 @@ public class Movement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed;
     public float jump;
+    public bool isFacingLeft;
     private float move;
     private Rigidbody2D rb;
+    Animator myAnimator;
     void Start()
     {
        rb = GetComponent<Rigidbody2D>(); 
+       myAnimator = GetComponent<Animator>();
+       isFacingLeft = false;
     }
 
     // Update is called once per frame
@@ -18,6 +22,14 @@ public class Movement : MonoBehaviour
         move = Input.GetAxis("Horizontal");
 
         rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        if (rb.linearVelocity.x != 0 && isFacingLeft) {
+            
+        } else if (rb.linearVelocity.x != 0 && !isFacingLeft) {
+
+        } else {
+
+        }
 
         if (Input.GetButtonDown("Jump")) {
             rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
