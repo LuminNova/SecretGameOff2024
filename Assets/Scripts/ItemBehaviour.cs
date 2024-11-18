9cf01e8890a71001564d87551f70597571cb2267
MaggieMa-1 <maggiema164@gmail.com>
2024-11-17 19:45:38 -0700

    Damaging item update and comments

--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -19,6 +19,7 @@ public class ItemBehaviour : MonoBehaviour
     void OnMouseDown(){
         Debug.Log("Mouse Clicked");
 
+        // Pickup item with tag PickupItem
         if ((player.position-gameObject.transform.position).magnitude < range && gameObject.CompareTag("PickupItem")) // Vector3.Distance() is also possible
         {
             if(InventoryManager.instance.AddItem(item)){
@@ -29,6 +30,7 @@ public class ItemBehaviour : MonoBehaviour
             }
         }
 
+        // Damage item and destroy when broken with (temporarily using) Respawn tag
         if ((player.position-gameObject.transform.position).magnitude < range && gameObject.CompareTag("Respawn")) // Vector3.Distance() is also possible
         {
             // Item being used
@@ -37,22 +39,26 @@ public class ItemBehaviour : MonoBehaviour
 
             Debug.Log("Item being used: " + useItem);
             if(useItem.isUsableItem){
-                // Destroy the item after pickup
                 Debug.Log("game object is being damaged");
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.left * 3, 1 * Time.deltaTime);
+
+                // Attempting to make item that is being hit, shake (it's not working)
+                //transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.left * 3, 1 * Time.deltaTime);
+                
                 item.durability = item.durability - useItem.damageNumber;
                 Debug.Log("New Item durability = " + item.durability);
+                
                 if(item.durability <= 0){
+                    // Reset item durability
                     item.durability = originalDurability;
                     Destroy(gameObject);
                 }
             } else {
-                Debug.Log("idk when i see this");
+                Debug.Log("Item is not useable");
             }
         }
     }
 
-
+    // When player (Player tag) touches an object
     // private void OnTriggerEnter2D(Collider2D other)
     // {
     //     // Check if the player is colliding with the item
