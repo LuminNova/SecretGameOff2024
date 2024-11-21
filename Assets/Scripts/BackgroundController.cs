5b8c367c81a2c0ae994a8782cb4cc81306d8076d
Anthony Dang <anthonydang230@gmail.com>
2024-11-21 16:05:50 -0700

    fixed the background from being just stationary

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private float startPos;
+    private float startPos, length;
     public GameObject cam;
     public float parallaxEffect; // speed the background should move relative to the camera
     void Start()
     {
         startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + length) {
+            startPos += length;
+        } else if ( movement < startPos - length) {
+            startPos -= length;
+        }
     }
 }
